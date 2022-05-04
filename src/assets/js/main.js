f54e1772acfc46d8ab2f0700078bc00d1c64245e
LucasKenjiValtech <lucas.kenji@valtech.com>
2022-05-04 10:28:47 -0300

    funcionalidade de adcionar elementos na lista to Do

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,8 +3,8 @@ var imgButtonModal;
 var modal;
 var taskInput;
 var addTaskButton;
-var taskList;
-var completedList
+var toDoList;
+var completedList;
 
 InitElements();
 
@@ -12,6 +12,11 @@ function InitElements() {
     buttonModal = document.getElementById("buttonModal");
     modal = document.getElementById("form-modal");
     imgButtonModal = document.querySelector("#buttonModal img");
+
+    taskInput = document.getElementById("taskInput");
+    addTaskButton = document.getElementById("addTask");
+
+    toDoList = document.querySelector("#to-do-itens .content")
 }
 
 function toggleModal(){
@@ -26,3 +31,54 @@ function toggleModal(){
         imgButtonModal.src = "src/assets/img/Plus.svg";
     }
 }
+
+function createTask(){
+    let inputValue = taskInput.value;
+    taskInput.value = ""
+
+    if(validateTask(inputValue)){
+        inspectList(inputValue)
+    }else{
+        alert("Digite algo antes de adicionar uma tarefa")
+    }
+    
+}
+
+function validateTask(value){
+    if(value.length > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function inspectList(value){
+    if(toDoList.children.length == 0){
+        insertElement(false, value, toDoList)
+    }
+    else{
+        insertElement(true, value, toDoList)
+    }
+}
+
+function insertElement(hasChild, value, list){
+    let task = document.createElement("label")
+    task.id = value;
+    task.innerHTML = `
+        <input type="checkbox">
+        <div>${value}</div>`
+    
+    if(hasChild){
+        for(i = 0; i < list.children.length; i++){
+            if(task.id < list.children[i].id){
+                list.children[i].before(task);
+            }
+        }
+    }else{
+        list.append(task);
+    }
+
+    toggleModal();
+    console.log(list)
+}
+
